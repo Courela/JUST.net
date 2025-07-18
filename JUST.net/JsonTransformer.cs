8d0d90647f3ae1894f018c5f2712494644fc93c7
Courela <courela@gmail.com>
2025-07-18 22:34:39 +0100

    Do not add previous alias to CurrentArrayToken when explicitly refer to other loop item as input for declaring one

--- JUST.net/JsonTransformer.cs
+++ JUST.net/JsonTransformer.cs
@@ -477,7 +477,10 @@ namespace JUST
             if (args.Length > 2)
             {
                 previousAlias = (string)ParseFunction(args[2].Trim(), state);
-                state.CurrentArrayToken.Add(new LevelKey() { Key = previousAlias, Level = _levelCounter }, Context.Input);
+                // if (!state.CurrentArrayToken.Any(t => t.Key.Key == previousAlias))
+                // {
+                //     state.CurrentArrayToken.Add(new LevelKey() { Key = previousAlias, Level = _levelCounter }, Context.Input);
+                // }
             }
             else if (state.CurrentArrayToken.Any(t => t.Key.Key == alias))
             {
